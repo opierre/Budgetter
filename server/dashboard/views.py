db1abcfe440eb42a7dc524445a94f202aa80c3ea
polivier2 <pierre.o@hotmail.fr>
2021-02-16 22:43:48 +0100

    Add category in models.py and update settings.py

--- server/dashboard/views.py
+++ server/dashboard/views.py
@@ -1,8 +1,7 @@
-from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 
-from dashboard.models import Transaction, Bank, Account
-from dashboard.serializers import TransactionSerializer, BankSerializer, AccountSerializer
+from dashboard.models import Transaction, Bank, Account, Category
+from dashboard.serializers import TransactionSerializer, BankSerializer, AccountSerializer, CategorySerializer
 
 
 class BankViewSet(ModelViewSet):
@@ -19,3 +18,12 @@ class TransactionViewSet(ModelViewSet):
     queryset = Transaction.objects.all()
     serializer_class = TransactionSerializer
 
+
+class CategoryViewSet(ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class TopCategoriesViewSet(ModelViewSet):
+    queryset = Category.objects.annotate()[:6]
+    serializer_class = TransactionSerializer
