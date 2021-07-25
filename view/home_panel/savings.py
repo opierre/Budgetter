4e72ce452584e6a5bf1889f146ea9bea74a20ef5
polivier2 <pierre.o@hotmail.fr>
2021-07-26 00:28:29 +0200

    Savings: last 12 months | Need to be fixed: values ordering

--- view/home_panel/savings.py
+++ view/home_panel/savings.py
@@ -21,6 +21,9 @@ class Savings(QObject):
         self.custom_status_bar = StatusBar()
         self.status_bar = QStatusBar()
 
+        """ Last 12 months button """
+        self.last_twelve_months = QPushButton("Last 12 months")
+
         """ Current Year button """
         self.current_year = QPushButton("")
 
@@ -50,6 +53,7 @@ class Savings(QObject):
         """
 
         """ Connect click on buttons to disaply new values """
+        self.last_twelve_months.clicked.connect(self.display_values)
         self.current_year.clicked.connect(self.display_values)
         self.previous_year.clicked.connect(self.display_values)
 
@@ -69,13 +73,23 @@ class Savings(QObject):
         if sender == self.current_year:
             """ Update stylesheet """
             self.current_year.setProperty("activated", "true")
+            self.last_twelve_months.setProperty("activated", "false")
             self.previous_year.setProperty("activated", "false")
 
             """ Update values to display """
             self.chart_widget.set_current_year_values(True)
+        elif sender == self.last_twelve_months:
+            """ Update stylesheet """
+            self.current_year.setProperty("activated", "false")
+            self.last_twelve_months.setProperty("activated", "true")
+            self.previous_year.setProperty("activated", "false")
+
+            """ Update values to display """
+            self.chart_widget.set_last_months()
         else:
             """ Update stylesheet """
             self.current_year.setProperty("activated", "false")
+            self.last_twelve_months.setProperty("activated", "false")
             self.previous_year.setProperty("activated", "true")
 
             """ Update values to display """
@@ -86,6 +100,8 @@ class Savings(QObject):
         self.current_year.style().polish(self.current_year)
         self.previous_year.style().unpolish(self.previous_year)
         self.previous_year.style().polish(self.previous_year)
+        self.last_twelve_months.style().unpolish(self.last_twelve_months)
+        self.last_twelve_months.style().polish(self.last_twelve_months)
 
     def configure_title_bar(self):
         """
@@ -112,14 +128,17 @@ class Savings(QObject):
         self.set_current_and_previous_year()
 
         """ Set states for activation """
-        self.current_year.setProperty("activated", "true")
+        self.current_year.setProperty("activated", "false")
         self.current_year.update()
         self.previous_year.setProperty("activated", "false")
         self.previous_year.update()
+        self.last_twelve_months.setProperty("activated", "true")
+        self.last_twelve_months.update()
 
         """ Set cursor for left buttons """
         self.current_year.setCursor(Qt.PointingHandCursor)
         self.previous_year.setCursor(Qt.PointingHandCursor)
+        self.last_twelve_months.setCursor(Qt.PointingHandCursor)
 
         """ Show next and previous arrows """
         self.custom_status_bar.show_previous_next()
@@ -128,6 +147,7 @@ class Savings(QObject):
         self.status_bar.addPermanentWidget(self.custom_status_bar)
 
         """ Add buttons on left corner """
+        self.status_bar.addWidget(self.last_twelve_months)
         self.status_bar.addWidget(self.current_year)
         self.status_bar.addWidget(self.previous_year)
 
--- widgets/saving_widgets/saving_dashboard_widget.py
+++ widgets/saving_widgets/saving_dashboard_widget.py
@@ -1,3 +1,4 @@
+from PySide2.QtCore import QDate
 from PySide2.QtWidgets import QWidget, QGridLayout
 
 from widgets.saving_widgets.callout_widget import CalloutChartView
@@ -12,31 +13,26 @@ class SavingDashboard(QWidget):
         super(SavingDashboard, self).__init__(parent)
 
         """ Store values for months """
-        self.values = [{"Janvier-2021": 12056,
-                        "Février-2021": 13450.12,
-                        "Mars-2021": 15469.35,
-                        "Avril-2021": 14356.00,
-                        "Mai-2021": 25098.63,
-                        "Juin-2021": 26098.57,
-                        "Juillet-2021": 22054.00,
-                        "Août-2021": 22000.45,
-                        "Septembre-2021": 29087.98,
-                        "Octobre-2021": 25043.23,
-                        "Novembre-2021": 25098.45,
-                        "Décembre-2021": 28034.00},
-                       {"Janvier-2020": 4235.23,
-                        "Février-2020": 4565.23,
-                        "Mars-2020": 5454.34,
-                        "Avril-2020": 5674.76,
-                        "Mai-2020": 7345.87,
-                        "Juin-2020": 8340.89,
-                        "Juillet-2020": 8957.54,
-                        "Août-2020": 11100.34,
-                        "Septembre-2020": 11550.12,
-                        "Octobre-2020": 11567.87,
-                        "Novembre-2020": 11978.78,
-                        "Décembre-2020": 12010.98}
-                       ]
+        self.values = {"Janvier-2021": 12056,
+                       "Février-2021": 13450.12,
+                       "Mars-2021": 15469.35,
+                       "Avril-2021": 14356.00,
+                       "Mai-2021": 25098.63,
+                       "Juin-2021": 26098.57,
+                       "Juillet-2021": 22054.00,
+                       "Août-2021": 22000.45,
+                       "Janvier-2020": 4235.23,
+                       "Février-2020": 4565.23,
+                       "Mars-2020": 5454.34,
+                       "Avril-2020": 5674.76,
+                       "Mai-2020": 7345.87,
+                       "Juin-2020": 8340.89,
+                       "Juillet-2020": 8957.54,
+                       "Août-2020": 11100.34,
+                       "Septembre-2020": 11550.12,
+                       "Octobre-2020": 11567.87,
+                       "Novembre-2020": 11978.78,
+                       "Décembre-2020": 12010.98}
 
         """ Store chart view """
         self.chart_view = CalloutChartView()
@@ -58,7 +54,7 @@ class SavingDashboard(QWidget):
         self._layout.addWidget(self.chart_view)
 
         """ Configure chart """
-        self.chart_view.set_values(self.values[0])
+        self.set_current_year_values(True)
 
     def set_current_year_values(self, boolean):
         """
@@ -68,7 +64,35 @@ class SavingDashboard(QWidget):
         :return: None
         """
 
+        values = dict()
+
         if boolean:
-            self.chart_view.set_values(self.values[0])
+            ''' Retrieve current year for parsing '''
+            year = str(QDate.currentDate().year())
+
         else:
-            self.chart_view.set_values(self.values[1])
+            ''' Retrieve previous year for parsing '''
+            year = str(QDate.currentDate().year() - 1)
+
+        for key, item in self.values.items():
+            if year in key:
+                values[key] = item
+        self.chart_view.set_values(values)
+
+    def set_last_months(self):
+        """
+        Display values for the last 12 months
+
+        :return: None
+        """
+
+        values = dict()
+
+        ''' Reconstrcut values '''
+        for key, item in self.values.items():
+            if len(values) != 12:
+                values[key] = item
+            else:
+                break
+
+        self.chart_view.set_values(values)
